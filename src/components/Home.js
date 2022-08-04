78558a8550f70b21f8299c831214283f46a2e0b4
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-08-04 08:54:24 -0400

    Fixed the header display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
+// import ArtifactsIndex from './artifacts/ArtifactsIndex'
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
+	const { msgAlert } = props
+
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2>See the Artifacts</h2>
+			{/* <ArtifactsIndex msgAlert={ msgAlert } /> */}
 		</>
 	)
 }
