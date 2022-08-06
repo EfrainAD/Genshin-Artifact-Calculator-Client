cd2d7648b819991cc2749109d53e66f757dcf146
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-08-06 14:10:19 -0400

    cleaned up the index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,24 +1,8 @@
-import { useState, useEffect } from 'react'
-import Card from 'react-bootstrap/Card'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import './style.css'
-
-import LoadingScreen from './shared/LoadingScreen'
-import { getAllArtifacts } from '../api/artifacts'
-// import { getAllCharacters } from '../api/characters'
-import messages from './shared/AutoDismissAlert/messages'
 
 import ArtifactsIndex from './artifacts/ArtifactsIndex'
 import CharactersIndex from './characters/CharactersIndex'
-
-// import {makeStyles} from '@material-ui/core/styles'
-import { buildQueries } from '@testing-library/react'
-
-// const useStyles = makeStyles(theme => {
-//     right-button: {
-//         background-color: "blue";
-//     }
-// })
 
 const Index = (props) => {
     const [filter, setFilter] = useState('artifacts') // or Characters
@@ -27,7 +11,6 @@ const Index = (props) => {
 
     console.log('Props in Index', props)
 
-////////////////
     if (filter === 'artifacts') {
         return (
             <>
@@ -43,7 +26,7 @@ const Index = (props) => {
                 <ArtifactsIndex msgAlert={ msgAlert } user={ user } />
             </>
         )
-    } else { // artifact === 'Character
+    } else { // filter === 'Character
         return (
             <>
                 <button className='button left-button'
@@ -59,8 +42,6 @@ const Index = (props) => {
             </>
         )
     }
-
-    
 }
 
 export default Index
